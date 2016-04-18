9a0889ae39a00491ec8c3f6f0f865add4983f873
黎城 <823354103@qq.com>
2016-04-18 14:35:19 +0800

    ImageCache
    自定义图片缓存

---
+++ src/main/java/com/example/asyncimageloader/Images.java
@@ -0,0 +1,18 @@
+package com.example.asyncimageloader;
+
+public class Images {
+
+	public final static String[] imageThumbUrls = new String[] {
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105906.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105854.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105843.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105836.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105828.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105820.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105810.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105803.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105755.jpg",
+			"http://www.diandidaxue.com:8080/images/beauty/20160117105749.jpg",
+			 };
+	
+}
